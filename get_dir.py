bd5ec4241f433968794d8f2f1bffc45c3f36b08e
Bachhu Manikanta <60522958+manikantasai164@users.noreply.github.com>
2024-02-24 12:22:06 +0530

    Update get_dir.py

--- get_dir.py
+++ get_dir.py
@@ -1,9 +1,9 @@
 import os
 
-
+# Include folder not required
 Exclude_list = [ 'Program Files','Program Files (x86)','AWS', 'Python', 'Python_Env', 'file_monitoring', 'done']
 
 for root, dirs, files in os.walk(r"C:\\Scripts\\"):
     dirs[:] = [d for d in dirs if d not in Exclude_list]
     for d in dirs:
-        print(os.path.join(root,d))
+        print(os.path.join(root,d))  # full path of the directory by excluding the Exclude_list
